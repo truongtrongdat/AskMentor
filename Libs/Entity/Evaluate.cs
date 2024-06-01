990c3e7e4ad7498e4a9c325b94cac3c708f33d72
doanhuyxh <doanhuyxh@gmail.com>
2024-06-01 21:12:17 +0700

    up

--- Libs/Entity/Evaluate.cs
+++ Libs/Entity/Evaluate.cs
@@ -6,7 +6,7 @@
         public int Rating { get; set; }
         public string UserId { get; set; }
         public string MentorId { get; set; }
-        public string Content { get; set; }
+        public string? Content { get; set; }
         public ApplicationUser User { get; set; }
     }
 }
